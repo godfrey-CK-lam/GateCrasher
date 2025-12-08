67ad6b8f512002ee6859c24e354cb9983de3e2cf
Lam, Godfrey Lam <gl2028@hw.ac.uk>
2025-12-08 12:57:20 +0000

    configure arg parsing

--- bot.py
+++ bot.py
@@ -1,1 +1,10 @@
-print("hello world!")
+
+import argparse
+
+parser = argparse.ArgumentParser()
+parser.add_argument('start', type=str)
+parser.add_argument('end', type=str)
+args = parser.parse_args()
+
+print(args.start)
+print(args.end)
