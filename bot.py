afa2125c164ed5ba8ced88dd696fb0c8ff226ec3
godfrey-CK-lam <godfreycklam@gmail.com>
2025-12-22 09:37:07 +0000

    reformat

--- bot.py
+++ bot.py
@@ -6,35 +6,37 @@ import datetime
 CONN = http.client.HTTPSConnection("esi.evetech.net")
 
 HEADERS = {
-        'Accept-Language': "en",
-        'If-None-Match': "",
-        'X-Compatibility-Date': "2025-12-16", #time.strftime("%Y-%m-%d")
-        'X-Tenant': "",
-        'Content-Type': "application/json",
-        'Accept': "application/json"
-    }
+    "Accept-Language": "en",
+    "If-None-Match": "",
+    "X-Compatibility-Date": "2025-12-16",  # time.strftime("%Y-%m-%d")
+    "X-Tenant": "",
+    "Content-Type": "application/json",
+    "Accept": "application/json",
+}
 
 # parse a system name to the id
+
+
 def get_id(system):
-    
-    payload =  "[\n  \"string\"\n]"
+
+    payload = '[\n  "string"\n]'
     myPayload = json.loads(payload)
     myPayload[0] = system
     payload = json.dumps(myPayload, indent=2)
-   
+
     CONN.request("POST", "/universe/ids", payload, HEADERS)
 
     res = CONN.getresponse()
     allData = res.read()
-    parseData = json.loads(allData)    
+    parseData = json.loads(allData)
 
     data = parseData.get("systems")
     return data[0]["id"]
 
-  
+
 # parse a system ID to a name
 def get_name(id):
-    payload =  "[\n  \"string\"\n]"
+    payload = '[\n  "string"\n]'
     myPayload = json.loads(payload)
     myPayload[0] = str(id)
     payload = json.dumps(myPayload, indent=2)
@@ -43,53 +45,55 @@ def get_name(id):
 
     res = CONN.getresponse()
     data = res.read()
-    jsonData = json.loads(data)    
-    return jsonData[0]['name']
+    jsonData = json.loads(data)
+    return jsonData[0]["name"]
+
 
 def get_info(id):
     CONN.request("GET", "/universe/systems/" + str(id), headers=HEADERS)
-    
+
     res = CONN.getresponse()
     data = res.read()
-    jsonData = json.loads(data)   
-    
-    return [jsonData['name'], round(jsonData['security_status'], 1), get_kills(id)]
+    jsonData = json.loads(data)
+
+    return [jsonData["name"], round(jsonData["security_status"], 1), get_kills(id)]
+
 
 def get_kills(id):
     CONN.request("GET", "/universe/system_kills", headers=HEADERS)
 
     res = CONN.getresponse()
     data = res.read()
-    parseData = json.loads(data)  
+    parseData = json.loads(data)
 
     kills = 0
-    result = next((obj for obj in parseData if obj["system_id"] == id),0) 
-    
+    result = next((obj for obj in parseData if obj["system_id"] == id), 0)
+
     if result:
         kills = result["pod_kills"] + result["ship_kills"]
     return kills
+
 
 def find_route(args):
     startid = get_id(args.start)
     endid = get_id(args.end)
 
-    payload = "{\n  \"avoid_systems\": [\n    30000001\n  ],\n  \"connections\": [\n    {\n      \"from\": 30000001,\n      \"to\": 30000001\n    }\n  ],\n  \"preference\": \"Shorter\",\n  \"security_penalty\": 50\n}"
+    payload = '{\n  "avoid_systems": [\n    30000001\n  ],\n  "connections": [\n    {\n      "from": 30000001,\n      "to": 30000001\n    }\n  ],\n  "preference": "Shorter",\n  "security_penalty": 50\n}'
     myPayload = json.loads(payload)
-    myPayload['preference'] = args.preference
-    myPayload['avoid_systems'] = args.avoid
+    myPayload["preference"] = args.preference
+    myPayload["avoid_systems"] = args.avoid
     payload = json.dumps(myPayload, indent=2)
 
-
-    
-    CONN.request("POST", "/route/"+str(startid)+"/"+str(endid)+"", payload, HEADERS)
+    CONN.request(
+        "POST", "/route/" + str(startid) + "/" +
+        str(endid) + "", payload, HEADERS
+    )
 
     res = CONN.getresponse()
     data = res.read()
-    theData = json.loads(data)    
+    theData = json.loads(data)
 
-
- 
-    idList = (theData['route'])
+    idList = theData["route"]
     route = []
 
     for i in range(len(idList)):
@@ -97,14 +101,30 @@ def find_route(args):
 
     return route
 
+
 def get_args():
-    parser = argparse.ArgumentParser(description="Displays information about a route from one system to another")
-    parser.add_argument('start', type=str,help=("The starting system"))
-    parser.add_argument('end', type=str, help="The destination system")
-    parser.add_argument('-p', '--preference', help="Route preferences", choices=["short","unsafe","safe"])
-    parser.add_argument('-a', '--avoid', type=str, help="avoid specific systems", action='store', nargs="+")
+    parser = argparse.ArgumentParser(
+        description="Displays information about a route from one system to another"
+    )
+    parser.add_argument("start", type=str, help=("The starting system"))
+    parser.add_argument("end", type=str, help="The destination system")
+    parser.add_argument(
+        "-p",
+        "--preference",
+        help="Route preferences",
+        choices=["short", "unsafe", "safe"],
+    )
+    parser.add_argument(
+        "-a",
+        "--avoid",
+        type=str,
+        help="avoid specific systems",
+        action="store",
+        nargs="+",
+    )
 
     return parser.parse_args()
+
 
 def transform(args):
 
@@ -112,9 +132,9 @@ def transform(args):
 
     if args.preference == "short":
         args.preference = "Shorter"
-    elif args.preference =="unsafe":
+    elif args.preference == "unsafe":
         args.preference = "LessSecure"
-    elif args.preference =="safe":
+    elif args.preference == "safe":
         args.preference = "Safer"
     else:
         args.preference = "Shorter"
@@ -125,8 +145,9 @@ def transform(args):
     else:
         args.avoid = [30000001]
 
-    #print(vars(args))
+    # print(vars(args))
     return args
+
 
 def main():
 
@@ -134,7 +155,6 @@ def main():
     args = transform(args)
     print(find_route(args))
 
- 
+
 if __name__ == "__main__":
     main()
-
